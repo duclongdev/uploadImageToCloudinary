8ba3f0c558eb9c8abda8e2b79718b7d6cca9189a
duclongdev <20521566@gm.uit.edu.vn>
2023-04-24 13:55:00 +0700

    first commit

---
+++ src/main/java/com/javawhizz/fileServer/config/CloudinaryConfig.java
@@ -0,0 +1,24 @@
+package com.javawhizz.fileServer.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+    private final String CLOUD_NAME = "dzvxrqimt";
+    private final String API_KEY = "448756346232223";
+    private final String API_SECRET = "6F3J3Lryryqhx26WwxIIIOwZDEk";
+    @Bean
+    public Cloudinary cloudinary(){
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name",CLOUD_NAME);
+        config.put("api_key",API_KEY);
+        config.put("api_secret",API_SECRET);
+
+        return new Cloudinary(config);
+    }
+}
